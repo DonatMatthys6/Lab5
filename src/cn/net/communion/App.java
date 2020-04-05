3a77936cacc703780a7b37a7991a2a48b17ff44b
donat <donat@10.18.15.234>
2020-04-04 22:15:49 -0700

    commit

--- src/cn/net/communion/App.java
+++ src/cn/net/communion/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println(new GeoHashHelper().around(45.0001, 116.3967));
         System.out.println(DistanceHepler.distance(44.9999, 116.3967, 45.0001, 116.3967));
         System.out.println("waste time: " + (System.currentTimeMillis() - start));
+        System.out.println("Hello from Donat");
         // long start = System.currentTimeMillis();
         // System.out.println(new GeoHashHelper().encode(44.9999, 116.3967));
         // System.out.println(new GeoHashHelper().encode(45.0001, 116.3967));
